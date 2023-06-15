bb25e189a2c2c30d5ef75b3f0348c9f30a24852a
e.adam <e.adam.cl@gmail.com>
2023-06-15 08:42:56 +0200

    páginas de transferencias

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransferenciasRecibidasComponent } from './transferencias-recibidas/transferencias-recibidas.component';
+import { TransferenciasEnviadasComponent } from './transferencias-enviadas/transferencias-enviadas.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'transferencias-recibidas',
+    component: TransferenciasRecibidasComponent,
+  },
+  {
+    path: 'transferencias-enviadas',
+    component: TransferenciasEnviadasComponent,
   },
 ];
 
---
+++ src/app/pages/transferencias-enviadas/transferencias-enviadas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { transferenciasEnviadas } from 'src/app/datos/transferencias-ejemplo';
+
+@Component({
+  selector: 'app-transferencias-enviadas',
+  templateUrl: './transferencias-enviadas.component.html',
+  styleUrls: ['./transferencias-enviadas.component.css'],
+})
+export class TransferenciasEnviadasComponent {
+  transferencias: any[] = transferenciasEnviadas;
+}
